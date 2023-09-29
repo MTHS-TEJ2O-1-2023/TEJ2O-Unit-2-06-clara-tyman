d68150ca70718566482711d450b145c711bd4746
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-09-29 17:16:17 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Clara
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear
+display.Icon.HAPPY
+pin16.write_digital(0)
+
+
+"Light On"
+display.scroll(str(button_a.get_presses()))
+display.Icon.YES
+pin16.write_digital(1)
+
+"Light off"
+display.scroll(str(button_b.get_presses()))
+display.Icon.NO
+pin16.write_digital(0)
